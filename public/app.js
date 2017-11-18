9bf8b4250662464e713b4fa0949e96d499ff122c
Danial Betres <danialbet@gmail.com>
2017-11-18 10:37:24 -0500

    fix firebase

--- public/app.js
+++ public/app.js
@@ -1,10 +1,12 @@
   // Initialize Firebase
+  
   var config = {
     apiKey: "AIzaSyDFhSeszWh8w9D-JNn5gAop9Auijd39Ak4",
-    authDomain: "musique-79bbb.firebaseapp.com",
     databaseURL: "https://musique-79bbb.firebaseio.com",
     projectId: "musique-79bbb",
-    storageBucket: "",
     messagingSenderId: "915472544803"
   };
   firebase.initializeApp(config);
+
+  var rootRef = firebase.database().ref();
+  
