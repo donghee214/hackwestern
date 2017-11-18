eb86198d2375ccd6b3bbcca9620e0f3b2e27c137
Danial Betres <danialbet@gmail.com>
2017-11-18 00:21:10 -0500

    firebase hosting v2

---
+++ public/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDFhSeszWh8w9D-JNn5gAop9Auijd39Ak4",
+    authDomain: "musique-79bbb.firebaseapp.com",
+    databaseURL: "https://musique-79bbb.firebaseio.com",
+    projectId: "musique-79bbb",
+    storageBucket: "",
+    messagingSenderId: "915472544803"
+  };
+  firebase.initializeApp(config);
