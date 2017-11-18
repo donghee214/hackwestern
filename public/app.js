dead1ba9c474966a9d8ff50e16749b3201fb8737
Kevin Sun <kevin_sun@hotmail.ca>
2017-11-18 10:41:10 -0500

    test2

--- public/app.js
+++ public/app.js
@@ -1,10 +1,28 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDFhSeszWh8w9D-JNn5gAop9Auijd39Ak4",
-    authDomain: "musique-79bbb.firebaseapp.com",
-    databaseURL: "https://musique-79bbb.firebaseio.com",
-    projectId: "musique-79bbb",
-    storageBucket: "",
-    messagingSenderId: "915472544803"
-  };
-  firebase.initializeApp(config);
+  (function(){
+    
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDFhSeszWh8w9D-JNn5gAop9Auijd39Ak4",
+      authDomain: "musique-79bbb.firebaseapp.com",
+      databaseURL: "https://musique-79bbb.firebaseio.com",
+      projectId: "musique-79bbb",
+      storageBucket: "",
+      messagingSenderId: "915472544803"
+    };
+    firebase.initializeApp(config);
+  
+    var rootRef = firebase.database().ref();
+  
+    //get element
+    const preObject = document.getElementById('object');
+  
+    //create references
+    const dbRefObject = firebase.database().ref().child('object');
+  
+    //sync object changes
+    dbRefObject.on('value', snap => console.log(snap.val()));
+  
+  
+  }());
+  
+  
